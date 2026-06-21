661ddad47abe1d6a40dae241c5fe86928bc5341d
beccaalise <beccaacathey@gmail.com>
2026-06-21 13:26:35 -0500

    Update character.js

--- character/character.js
+++ character/character.js
@@ -1,2 +1,9 @@
 //Rebecca Cathey, June 21st 2026
 //Adventure Character Creator
+
+//Step 4: Welcome Message
+alert(`Welcome to the Adventure Character Creator!
+My name is Rebecca, and today we'll create a legendary hero who lives in a magical Underwater Kingdom. 
+Our kingdom is full of coral castles, hidden treasures, and mysterious sea creatures that need protecting.`);
+
+//Step 5: Character Name
