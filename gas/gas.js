3103e8f6b2d54d6633aa674af335e2ec9862006c
beccaalise <beccaacathey@gmail.com>
2026-07-12 22:18:54 -0500

    Update gas.js

--- gas/gas.js
+++ gas/gas.js
@@ -11,11 +11,19 @@ let username = promt("Enter your name");
 if (username === null) {
   alert("You cancelled the name prompt";
 }
-else if (username,trim() === "") {
+else if (username.trim() === "") {
   alert ("You must type a name")
 }
 
-//Use an else that use a variable average
+//Use an else that use a variable average and check cancelled entry
+else{
+  let average = calcGasAvg():
+  if (average === null) {
+    alert("You cancelled entering gas totals.")
+  }
+}
+
+// Show gas totals or averages
 else {
   if (entryCount === 0) {
     alert("Mo gas totals were entered.");
@@ -26,4 +34,22 @@ else {
   else {
     alert(username + "'s average weekly gas bill is $" + average.toFixed(2));
   }
+
+  let entryMessage = "";
+  if (entryCount === 0) {
+    entryMessage = "No gas totals were entered.";
+  }
+  else if (entryCount === 1) {
+    entryMessage = "You entered 1 gas total.";
+  }
+  else {
+    entryMessage = "You entered " + entryCount + "gas totals.";
+  }
+
+  alert (entryMessage);
+}
+
+//Calculate the average gas bill
+Function calcGasAvg () {
+  let input;
 }
